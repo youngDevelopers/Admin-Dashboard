d018cbdcf37fbef238c0eb8646c590c200400a90
youngDevelopers <dambaemmanuel5@gmail.com>
2023-03-18 12:55:37 +0200

    Setting the Columns for the DataGrid Table

--- src/scenes/invoices/Invoices.jsx
+++ src/scenes/invoices/Invoices.jsx
@@ -13,7 +13,26 @@ function Invoices() {
         {
             field: 'id',
             headerName: 'ID',
-        }
+        },
+
+        {
+            field: "name",
+            headerName: "Name",
+            flex: 1,
+            cellClassName: "name-column--cell",
+        },
+
+        {
+            field: "phone",
+            headerName: "Phone Number",
+            flex: 1,
+        },
+        
+        {
+            field: "email",
+            headerName: "Email",
+            flex: 1,
+        },
     ]
 
     return (
