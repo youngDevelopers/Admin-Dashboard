04862aad203b135c4c4ad3a252c16c2e65a686b5
youngDevelopers <dambaemmanuel5@gmail.com>
2023-03-18 13:09:03 +0200

    Final Invoices Page

--- src/scenes/invoices/Invoices.jsx
+++ src/scenes/invoices/Invoices.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Typography, useTheme } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import { mockDataInvoices } from "../../data/mockData";
 import Header from "../../components/Header";
@@ -27,21 +27,72 @@ function Invoices() {
             headerName: "Phone Number",
             flex: 1,
         },
-        
+
         {
             field: "email",
             headerName: "Email",
             flex: 1,
         },
+
+        {
+            field: "cost",
+            headerName: "Cost",
+            flex: 1,
+            renderCell: (params) => {
+                return(
+                    <Typography color={colors.greenAccent[500]} >
+                        $USD {params.row.cost}
+                    </Typography>
+                )
+            }
+        },
+
+        {
+            field: "date",
+            headerName: "Date",
+            flex: 1,
+          },
     ]
 
     return (
         <Box m="40px">
-            <Header title="INVOICES" subtitle="Invoices List" />
-            <Box >
+            <Header title="INVOICES" subtitle="List of Invoice Balances" />
+            <Box 
+                m="40px 0 0 0"
+                height="75vh"
+                sx={{
+                  "& .MuiDataGrid-root": {
+                    border: "none",
+                  },
+                  "& .MuiDataGrid-cell": {
+                    borderBottom: "none",
+                  },
+                  "& .name-column--cell": {
+                    color: colors.greenAccent[300],
+                  },
+                  "& .MuiDataGrid-columnHeaders": {
+                    backgroundColor: colors.blueAccent[700],
+                    borderBottom: "none",
+                  },
+                  "& .MuiDataGrid-virtualScroller": {
+                    backgroundColor: colors.primary[400],
+                  },
+                  "& .MuiDataGrid-footerContainer": {
+                    borderTop: "none",
+                    backgroundColor: colors.blueAccent[700],
+                  },
+                  "& .MuiCheckbox-root": {
+                    color: `${colors.greenAccent[200]} !important`,
+                  },
+                  "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                    color: `${colors.grey[100]} !important`,
+                  },
+                }}
+            >
                 <DataGrid 
                     rows={mockDataInvoices}
                     columns={columns}
+                    components={ { Toolbar: GridToolbar } }
                 />
             </Box>
         </Box>
