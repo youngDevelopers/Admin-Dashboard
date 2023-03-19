af9d411bd3e12c270eb71378d25017d8fbf54813
youngDevelopers <dambaemmanuel5@gmail.com>
2023-03-19 12:31:27 +0200

    Setting Calender Fuctionality by setting Handlers

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
             </Routes>
 
           </main>
---
+++ src/scenes/calendar/Calendar.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+
+function Calendar(){
+
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [currentEvents, setCurrentEvents] = useState([]);
+
+    const handleDateClick = (selected) => {
+        console.log(selected);
+        const title = prompt("Please enter a new title for your event");
+        const calendarApi = selected.view.calendar;
+        calendarApi.unselect();
+    
+        if (title) {
+          calendarApi.addEvent({
+            id: `${selected.dateStr}-${title}`,
+            title,
+            start: selected.startStr,
+            end: selected.endStr,
+            allDay: selected.allDay,
+          });
+        }
+      };
+    
+      const handleEventClick = (selected) => {
+        if (
+          window.confirm(
+            `Are you sure you want to delete the event '${selected.event.title}'`
+          )
+        ) {
+          selected.event.remove();
+        }
+      };
+
+    return(
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default Calendar;
